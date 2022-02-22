a84b58de598751b1b475d215d6db52cdb5709d7e
Juho Poutiainen <j.poutiainen94@gmail.com>
2022-02-22 16:51:47 +0200

    Add functions to display input form and eventlistener to show all the books after the input

--- script/script.js
+++ script/script.js
@@ -1,3 +1,6 @@
+const bookCard = document.querySelector('.book-container');
+const bookValues = document.getElementById("add-book");
+
 let myLibrary = [];
 
 function Book(author, title, pages, read) {
@@ -12,16 +15,43 @@ function addBookToLibrary(newBook) {
     myLibrary.push(newBook);
 }
 
+// displaying all the books on the page
 function displayBooks() {
+    document.querySelectorAll('#book-card').forEach(e => e.remove());
+
     myLibrary.forEach((book) => {
         let element = document.createElement('div');
+        element.setAttribute("id", "book-card");
         let textElement = document.createElement('p');
         textElement.textContent = "Author: " + book.author +
             " Title: " + book.title + " Pages: " + book.pages +
             " Book read? " + book.read;
         element.appendChild(textElement);
-        document.body.appendChild(element);
+        bookCard.appendChild(element);
     })
 }
 
-displayBooks();
+// displays & hides the form to input Book details
+function displayForm() {
+    let form = document.querySelector('.form-container');
+    if (form.style.display === "none") {
+        form.style.display = 'block';
+    } else {
+        form.style.display = 'none';
+    }
+}
+
+// Calls new Book with values from the input form
+function formValues() {
+    let author = document.getElementById('author').value;
+    let title = document.getElementById('title').value;
+    let pages = document.getElementById('pages').value;
+    let read = document.getElementById('read').value;
+    newBook = new Book(author, title, pages, read);
+}
+
+bookValues.addEventListener("click", () => {
+    formValues();
+    displayForm();
+    displayBooks();
+ });
