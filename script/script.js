d3472a7fc4b77e82349acbada88f86d0f2f9d576
Juho Poutiainen <j.poutiainen94@gmail.com>
2022-02-23 13:33:25 +0200

    Add functions to delete books and to change book's read status

--- script/script.js
+++ script/script.js
@@ -1,4 +1,4 @@
-const bookCard = document.querySelector('.book-container');
+const bookContainer = document.querySelector('.book-container');
 const bookValues = document.getElementById("add-book");
 
 let myLibrary = [];
@@ -15,20 +15,36 @@ function addBookToLibrary(newBook) {
     myLibrary.push(newBook);
 }
 
-// displaying all the books on the page
+// displaying all the books on the page & calls function to delete books if remove button is pressed
 function displayBooks() {
     document.querySelectorAll('#book-card').forEach(e => e.remove());
 
-    myLibrary.forEach((book) => {
+    myLibrary.forEach((book, index) => {
         let element = document.createElement('div');
         element.setAttribute("id", "book-card");
         let textElement = document.createElement('p');
-        textElement.textContent = "Author: " + book.author +
+        let bookIndex = index + 1;
+        textElement.textContent = "Book: " + bookIndex + " Author: " + book.author +
             " Title: " + book.title + " Pages: " + book.pages +
             " Book read? " + book.read;
         element.appendChild(textElement);
-        bookCard.appendChild(element);
+        let removeButton = document.createElement('button');
+        removeButton.setAttribute('id', 'remove-button');
+        removeButton.textContent = "Remove Book"
+        removeButton.setAttribute('data-item-index', index);
+        element.appendChild(removeButton);
+
+        let statusButton = document.createElement('button');
+        statusButton.setAttribute('id', 'status-button');
+        statusButton.textContent = "Change status";
+        statusButton.setAttribute('data-item-index', index);
+        element.appendChild(statusButton);
+
+        bookContainer.appendChild(element);
     })
+
+    deleteBooks();
+    changeStatus();
 }
 
 // displays & hides the form to input Book details
@@ -40,6 +56,41 @@ function displayForm() {
         form.style.display = 'none';
     }
 }
+
+//deletes books and displays remaining books 
+function deleteBooks() {
+    let deleteButton = document.querySelectorAll('#remove-button');
+    deleteButton.forEach(button => {
+        button.addEventListener('click', () => {
+            let dataIndex = button.getAttribute('data-item-index');
+            myLibrary.splice(dataIndex, 1);
+            displayBooks();
+        })
+    })
+}
+
+function changeStatus() {
+    let statusButton = document.querySelectorAll('#status-button');
+    statusButton.forEach(button => {
+        button.addEventListener('click', () => {
+            let dataIndex = button.getAttribute('data-item-index');
+            console.log(dataIndex);
+            let change = myLibrary[dataIndex];
+            console.log(change.read);
+            if (change.read === "Yes") {
+                change.read = "No";
+                displayBooks();
+                console.log(change.read);
+            } else {
+                change.read = "Yes";
+                displayBooks();
+            }
+            
+            
+        })
+    })
+}
+
 
 // Calls new Book with values from the input form
 function formValues() {
@@ -54,4 +105,4 @@ bookValues.addEventListener("click", () => {
     formValues();
     displayForm();
     displayBooks();
- });
+});
